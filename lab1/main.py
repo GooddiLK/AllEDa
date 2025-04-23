4af940f1b86b2959d828443884b9337d75f6f797
ed <kesha.ek@gmail.com>
2025-04-24 02:35:28 +0300

    done quasi-newtonian method + Newton-CG

--- lab1/main.py
+++ lab1/main.py
@@ -1,17 +1,21 @@
 import math
 
 import numpy as np
-from matplotlib.colors import LinearSegmentedColormap
+import sympy as sp
 
-from GradientDescent import GradientDescent
+from matplotlib.colors import LinearSegmentedColormap
 from numpy import arange
 from numpy import meshgrid
 from matplotlib import pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+
+from GradientDescent import GradientDescent, GradientDescentWithScipy
 from LearningRateScheduling import LearningRateSchedulingConstant, LearningRateSchedulingGeom, \
     LearningRateSchedulingExponential, LearningRateSchedulingPolynomial
 from OneDimensional import Armijo, Wolfe
 from StoppingCriteria import Iterations, SequenceEps, SequenceValueEps
+from utils import *
+
+x, y = sp.symbols('x y')
 
 func_table = [
     [lambda x: x[0] ** 2 - 10, lambda x: 2 * x[0]],
@@ -27,6 +31,11 @@ func_table = [
                 2 * (x[0] + x[1] ** 2 - 7) * (2 * x[1]) + 2 * (x[0] ** 2 + x[1] - 11)]],
 ]
 
+sympy_func = [
+    x**2 + y**2,
+    3 * x ** 2 + 10 * y ** 2 - 4 * x * y,
+    (x ** 2 + y - 11) ** 2 + (x + y ** 2 - 7) ** 2
+]
 
 def print_res(gd_inst, point, iterations):
     r = gd_inst(point, iterations)
@@ -91,6 +100,16 @@ if __name__ == "__main__":
     # run(3, Wolfe(a, c1, c2, e), SequenceValueEps(0.0001), test_point, iter_max)
     # run(f_num, Wolfe(1, 0.001, 0.1, 0.0001), SequenceValueEps(0.0001), test_point, iter_max)
 
-    #
-    gd = GradientDescent(func_table[2][0], func_table[2][1], Wolfe(12, 0.001, 0.1, 0.0001), SequenceEps(10 ** -6))
-    show(func_table[3][0], rng, rng/20, 300, gd([3, 3], 5 * 10**3)[0])
+    # Newton-CG с использованием scipy
+    # gd_scipy = GradientDescentWithScipy(func=sympy_func[2] , method="Newton-CG")
+
+    # BFGS с использованием scipy
+    # gd_scipy = GradientDescentWithScipy(func=sympy_func[2], method="BFGS")
+
+    # res = gd_scipy((10, 10), iter_max)
+    # show(func_table[3][0], rng, rng/20, 300, gd_scipy(startPoint=test_point, iterations=iter_max)[0])
+
+
+
+    gd = GradientDescent(func_table[3][0], func_table[3][1], Wolfe(12, 0.001, 0.1, 0.0001), SequenceEps(10 ** -6))
+    show(func_table[3][0], rng, rng/20, 300, gd(startPoint=test_point, iterations=5 * 10**3)[0])
