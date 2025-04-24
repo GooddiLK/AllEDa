5258a0c5a90a2ef38483e2241986d9956984ca2c
sindat <dimakuleb18@gmail.com>
2025-04-24 21:29:42 +0300

    newtoneMethod -- only dog_leg realisation needed, other things - done (also needs to be tested after final realisation)

--- lab2/NewtoneMethod.py
+++ lab2/NewtoneMethod.py
@@ -1,12 +1,23 @@
 import numpy as np
 
 
-def get_model(function, gradient, hess, xk):
-    return lambda p: function(xk) + gradient @ p + (p.transpose @ hess @ p) / 2
+def get_model(dot_result, gradient, hess):
+    return lambda p: dot_result + gradient @ p + (p.transpose @ hess @ p) / 2
 
 
-def newtoneMethodStart(function, gradient_matrix_function, hess_matrix_function, x0=0, delta=1,
-                       iteration_stop_limit=1e-5, max_iter: int = 100_000, learning_rate=1):
+def newtoneMethodStart(
+        function,
+        gradient_matrix_function,
+        hess_matrix_function,
+        x0=0,
+        delta=1,
+        iteration_stop_limit=1e-5,
+        max_iter: int = 100_000,
+        learning_rate=1,
+        trust_upper_bound=3 / 4,
+        trust_lower_bound=1 / 4,
+        trust_no_trust_bound=1 / 16,
+        trust_changing_multiply_value=2):
     assert max_iter > 0
     assert iteration_stop_limit > 0
     assert delta > 0
@@ -14,18 +25,32 @@ def newtoneMethodStart(function, gradient_matrix_function, hess_matrix_function,
     cur_iter_number = 0
 
     prev_x = x0
-    prev_result = function(prev_x)
     cur_x = x0 + 1
     cur_result = function(cur_x)
 
     assert iteration_stop_limit < cur_x - prev_x
-    while cur_x - prev_x < iteration_stop_limit and max_iter < cur_iter_number:
+    while np.linalg.norm(cur_x - prev_x) < iteration_stop_limit and max_iter < cur_iter_number:
         gradient = gradient_matrix_function(cur_x)
         hess = hess_matrix_function(cur_x)
-        np.array([[1, 2], [4, 5]])
-        hess_reversed = hess.__invert__()
-        model = get_model(function, gradient, hess, cur_x)
+        hess_reversed = hess.__invert__()  # переделать потом можно -- вместо этого решать систему линейных уравнений
+        model = get_model(cur_result, gradient, hess)
         p = -hess_reversed @ gradient
         C_dot = cur_x + learning_rate * p
-        A_dot = dog_leg(model, C_dot, gradient, delta) #TODO dog leg
+        is_trusted = False
 
+        while not is_trusted:
+            A_dot = dog_leg(model, C_dot, gradient, delta)  # TODO dog leg
+            p_k = (cur_result - function(A_dot)) / (model(0) - model(A_dot - cur_x))
+            if p_k > trust_upper_bound:
+                delta *= trust_changing_multiply_value
+            elif p_k > trust_lower_bound:
+                delta = delta
+            elif p_k > trust_no_trust_bound:
+                delta /= trust_changing_multiply_value
+            else:
+                delta /= trust_changing_multiply_value
+                is_trusted = False
+                continue
+            is_trusted = True
+            prev_x = cur_x
+            cur_x = A_dot
