5cd7db220ae711fc8f24ece20d300e0e71c21103
AlexOne007 <alex.am.chaos@gmail.com>
2025-04-24 21:41:53 +0300

    optuna

--- lab1/main.py
+++ lab1/main.py
@@ -1,86 +1,19 @@
-import math
-
-import numpy as np
-from matplotlib.colors import LinearSegmentedColormap
-
 from GradientDescent import GradientDescent
-from numpy import arange
-from numpy import meshgrid
-from matplotlib import pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from LearningRateScheduling import LearningRateSchedulingConstant, LearningRateSchedulingGeom, \
-    LearningRateSchedulingExponential, LearningRateSchedulingPolynomial
-from OneDimensional import Armijo, Wolfe
-from StoppingCriteria import Iterations, SequenceEps, SequenceValueEps
+from LearningRateScheduling import *
+from OneDimensional import *
+from StoppingCriteria import *
 
-func_table = [
-    [lambda x: x[0] ** 2 - 10, lambda x: 2 * x[0]],
-    # lambdas = ((a+c) +- sqrt( (a-c)^2 + 4b^2 ))/2
-    # x^2 + y^2; a = 1, b = 0, c = 1; lambdas = 1, 1; Число обусловленности = 1
-    [lambda x: x[0] ** 2 + x[1] ** 2, lambda x: [2 * x[0], 2 * x[1]]],
-    # 3x^2 - 4xy + 10y^2; a = 3; b = -4; c = 10; lambdas = 2.3699, 23.63; Число обусловленности = 9,97
-    [lambda x: 3 * x[0] ** 2 + 10 * x[1] ** 2 - 4 * x[0] * x[1],
-     lambda x: [6 * x[0] - 4 * x[1], 20 * x[1] - 4 * x[0]]],
-
-    [lambda x: (x[0] ** 2 + x[1] - 11) ** 2 + (x[0] + x[1] ** 2 - 7) ** 2,
-     lambda x: [2 * (x[0] ** 2 + x[1] - 11) * (2 * x[0]) + 2 * (x[0] + x[1] ** 2 - 7),
-                2 * (x[0] + x[1] ** 2 - 7) * (2 * x[1]) + 2 * (x[0] ** 2 + x[1] - 11)]],
-]
-
-
-def print_res(gd_inst, point, iterations):
-    r = gd_inst(point, iterations)
-    print("кол-во итераций + конечная точка")
-    print(len(r[0]), r[0][-1])
-    print("кол-во вызовов функции и ее производной")
-    print(r[1:])
-    print("---------------------------------------")
-
-
-def run(func_number, learning_rate, stopping_criteria, point, iterations):
-    gd = GradientDescent(func_table[func_number][0], func_table[func_number][1], learning_rate, stopping_criteria)
-    print_res(gd, point, iterations)
-
-def to2(func):
-    return lambda x, y: func([x, y])
-
-
-def show(func, rng, grid, last_points, r):
-    if last_points > 0:
-        r = r[-last_points:]
-    lx, ly = r[-1]
-    xaxis = arange(-rng + lx, rng + lx, grid)
-    yaxis = arange(-rng + ly, rng + ly, grid)
-    x, y = meshgrid(xaxis, yaxis)
-    results = to2(func)(x, y)
-    figure = plt.figure()
-    axis = figure.add_subplot(111, projection='3d')
-    axis.plot_surface(x, y, results, cmap='viridis', alpha=0.5)
-    rx = np.array([i[0] for i in r])
-    ry = np.array([i[1] for i in r])
-    rz = to2(func)(rx, ry)
-    indices = np.linspace(0, 1, len(r))
-    indices = np.array([math.exp(-i) for i in indices])
-    colors = ["green", "red"]
-    cmap_custom = LinearSegmentedColormap.from_list("RedToGreen", colors)
-    axis.scatter(
-        rx, ry, rz,
-        c=indices,
-        cmap=cmap_custom,
-    )
-    plt.show()
-
+from Examples import func_table
+from Pyplot import show
 
 if __name__ == "__main__":
-    rng = 2
-
     f_num = 1
     test_point = [np.longdouble(10), np.longdouble(10)]
     iter_max = 10 ** 4
     #
     # run(f_num, LearningRateSchedulingConstant(0.025), SequenceValueEps(0.0001), test_point, iter_max)
     # run(f_num, LearningRateSchedulingGeom(1.1, 1/5), SequenceValueEps(0.0001), test_point, iter_max)
-    # run(f_num, LearningRateSchedulingExponential(0.001), SequenceValueEps(0.0001), test_point, iter_max)
+    # run(f_num, LearningRateSchedulingExponential(1, 0.001), SequenceValueEps(0.0001), test_point, iter_max)
     # run(f_num, LearningRateSchedulingPolynomial(0.5, 2), SequenceValueEps(0.0001), test_point, iter_max)
     # a = 1
     # c1 = 0.001
@@ -93,4 +26,6 @@ if __name__ == "__main__":
 
     #
     gd = GradientDescent(func_table[2][0], func_table[2][1], Wolfe(12, 0.001, 0.1, 0.0001), SequenceEps(10 ** -6))
+    # graphic:
+    rng = 2
     show(func_table[3][0], rng, rng/20, 300, gd([3, 3], 5 * 10**3)[0])
---
+++ lab2/Optuna.py
@@ -0,0 +1,62 @@
+import numpy as np
+import optuna as opt
+import math
+from random import randint
+
+from lab1.Examples import *
+from lab1.GradientDescent import GDException
+from lab1.LearningRateScheduling import LearningRateSchedulingConstant, LearningRateSchedulingExponential
+from lab1.StoppingCriteria import *
+
+max_iterations = 5 * 10 ** 3
+eps = 10 ** -4
+stopping_criteria = SequenceEps(eps)
+very_big_constant = 1_000_000_000_000_000_000
+
+
+def optimizing_func(func_calc, grad_calc, found, real):
+    return func_calc + grad_calc + very_big_constant * np.linalg.norm(real - found)
+
+
+def run_study(objective, n_trials=50):
+    study = opt.create_study(direction="minimize")
+    study.optimize(objective, n_trials=n_trials)
+    print("Лучшие параметры:", study.best_params)
+
+
+# Runs objective with provided gradient descent and point it should find
+def run_objective(gd, real, delta=50, number_of=40):
+    dims = len(real)
+    real = np.array(real).astype(np.longdouble)
+    result = 0
+    for i in range(number_of):
+        point = [0] * dims
+        for j in range(dims):
+            point[j] = randint(int(real[j] - delta), int(real[j] + delta))
+        point = np.array(point).astype(np.longdouble)
+        try:
+            h, func_calc, grad_calc = run(gd, point, max_iterations)
+        except GDException as _:
+            result += very_big_constant ** 2
+            continue
+        one_result = optimizing_func(func_calc, grad_calc, h[-1], real)
+        result += one_result
+    return result
+
+
+# Optimizing constant learning rate on function 1
+def objective1(trial):
+    learning_rate_0 = trial.suggest_float('learning_rate', 1e-10, 1, log=True)
+    learning_rate = LearningRateSchedulingConstant(learning_rate_0)
+    gd = grad_des_instance(1, learning_rate, stopping_criteria)
+    return run_objective(gd, [0, 0])
+
+
+# Optimizing exponential learning rate on function 2
+def objective2(trial):
+    h0 = trial.suggest_float('h0', 1e-10, 1, log=True)
+    lambda_param = trial.suggest_float('lambda_param', 1e-10, 1, log=True)
+    learning_rate = LearningRateSchedulingExponential(h0, lambda_param)
+    gd = grad_des_instance(2, learning_rate, stopping_criteria)
+    return run_objective(gd, [0, 0])
+
