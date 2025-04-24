f204fb56151b91d18557ff359372142e2a0f43ae
AlexOne007 <alex.am.chaos@gmail.com>
2025-04-24 21:41:53 +0300

    optuna

---
+++ lab1/Examples.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+from lab1.GradientDescent import GradientDescent
+
+
+func_table = [
+    [lambda x: x[0] ** 2 - 10, lambda x: 2 * x[0]],
+    # lambdas = ((a+c) +- sqrt( (a-c)^2 + 4b^2 ))/2
+    # x^2 + y^2; a = 1, b = 0, c = 1; lambdas = 1, 1; Число обусловленности = 1
+    [lambda x: x[0] ** 2 + x[1] ** 2, lambda x: [2 * x[0], 2 * x[1]]],
+    # 3x^2 - 4xy + 10y^2; a = 3; b = -4; c = 10; lambdas = 2.3699, 23.63; Число обусловленности = 9,97
+    [lambda x: 3 * x[0] ** 2 + 10 * x[1] ** 2 - 4 * x[0] * x[1],
+     lambda x: [6 * x[0] - 4 * x[1], 20 * x[1] - 4 * x[0]]],
+
+    [lambda x: (x[0] ** 2 + x[1] - 11) ** 2 + (x[0] + x[1] ** 2 - 7) ** 2,
+     lambda x: [2 * (x[0] ** 2 + x[1] - 11) * (2 * x[0]) + 2 * (x[0] + x[1] ** 2 - 7),
+                2 * (x[0] + x[1] ** 2 - 7) * (2 * x[1]) + 2 * (x[0] ** 2 + x[1] - 11)]],
+]
+
+
+def grad_des_instance(func_number, learning_rate, stopping_criteria):
+    gd = GradientDescent(func_table[func_number][0], func_table[func_number][1], learning_rate, stopping_criteria)
+    return gd
+
+
+def run(gd, point, iterations):
+    return gd(point, iterations)
+
+
+def print_res(gd_inst, point, iterations):
+    r = run(gd_inst, point, iterations)
+    print("кол-во итераций + конечная точка")
+    print(len(r[0]), r[0][-1])
+    print("кол-во вызовов функции и ее производной")
+    print(r[1:])
+    print("---------------------------------------")
---
+++ lab2/main.py
@@ -0,0 +1,5 @@
+from Optuna import *
+
+if __name__ == "__main__":
+    # run_study(objective1)
+    run_study(objective2)
