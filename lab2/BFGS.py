9137374b511f56d71b55f0caec4a4c4b2e355f52
ed <kesha.ek@gmail.com>
2025-05-03 22:39:25 +0300

    fix bfgs

--- lab2/BFGS.py
+++ lab2/BFGS.py
@@ -9,26 +9,25 @@ class BFGS:
         variables = sp.symbols('x y')
         self.grad = compute_gradient(func, variables)
         self.bctrk = Backtracking(alpha_0, q, eps, c1)
-        self.hess = np.eye(dem)
+        self.hess = np.eye(dem, dtype=np.longdouble)
 
     def learning_rate(self, gd):
         x = gd.history()[-1]
-        x = np.array(x)
+        x = np.array(x, dtype=np.longdouble)
         cur_grad = self.grad(*x)
         gd.vector = -self.hess @ cur_grad
 
         a = self.bctrk.calculate_alpha(gd, x)
 
-        s = a * gd.vector
-        x_new = x + s
-        delta_grad = np.array(self.grad(*x_new)) - cur_grad
+        s = np.array(a * gd.vector, dtype=np.longdouble)
+        x_new = np.array(x + s, dtype=np.longdouble)
+        delta_grad = np.array(self.grad(*x_new), dtype=np.longdouble) - cur_grad
 
-        rho = 1.0 / np.dot(delta_grad, s)
-        i = np.eye(len(s))
+        rho = np.longdouble(1.0) / np.dot(delta_grad, s)
+        i = np.eye(len(s), dtype=np.longdouble)
         a1 = i - rho * s[:, np.newaxis] * delta_grad[np.newaxis, :]
         a2 = i - rho * delta_grad[:, np.newaxis] * s[np.newaxis, :]
 
-        self.hess = np.dot(a1, np.dot(self.hess, a2)) + (self.hess * s[:, np.newaxis] *
-                                           s[np.newaxis, :])
+        self.hess = np.dot(a1, np.dot(self.hess, a2)) + (rho * s[:, np.newaxis] * s[np.newaxis, :])
 
         return a
